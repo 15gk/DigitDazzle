858ae4b6920f9e59106306e1d98a44cba651a552
15gk <gautamkriti89@gmail.com>
2023-10-05 22:38:47 +0545

    added basic outline

---
+++ mycalc/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Operation from './components/Operation'
+
+const App = () => {
+  return (<>
+    <h1> Welcome to the Calculator </h1>
+    <button>Proceed</button>
+    <Operation/>
+    </>
+  )
+}
+
+export default App
---
+++ mycalc/src/components/Operation.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Operation = () => {
+  return (<>
+  <br/>
+<input type="text" id="fname" name="fname"></input>
+    <button>9</button>
+    <button>8</button>
+    <button>7</button>
+    <button>6</button>
+    <button>5</button>
+    <button>4</button>
+    <button>3</button>
+    <button>2</button>
+    <button>1</button>
+    <button>0</button>
+    <button>.</button>
+    <button>+</button>
+    <button>-</button>
+    <button>/</button>
+    <button>*</button>
+    <button>%</button>
+    <button>.</button>
+    <button>Back</button>
+        </>
+  )
+}
+
+export default  Operation
